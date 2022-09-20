36bb88fe5187b9320cc6a713d3f595765b708f28
Shubham <Shubham@DESKTOP-NICD8D9>
2022-09-20 15:20:22 +0530

    My first program on githup

---
+++ src/Programs/Welcome.java
@@ -0,0 +1,11 @@
+package Programs;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		System.out.print("welcome to my world");
+		// TODO Auto-generated method stub
+
+	}
+
+}
